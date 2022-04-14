1573c934a26787e1ae712e0ec97d9f81d2b7a88d
Krindha <86869866+Krindha@users.noreply.github.com>
2022-04-14 10:55:51 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,18 +1,18 @@
 module.exports = {
-  Admins: ["708983835018395679", "842620032960823327"], //Admins of the bot
+  Admins: ["708983835018395679"], //Admins of the bot
   ExpressServer: true, //If you wanted to make the website run or not
   DefaultPrefix: "?", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/msapnkyn", //Support Server Link
-  Token: process.env.Token || "OTYxODI3MTA5MDEyMTM1OTY2.Yk-pTA.uQ_9kx4zLde99IdT758y6vl0jVo", //Discord Bot Token
+  Token: process.env.Token || "OTYxODI3MTA5MDEyMTM1OTY2.Yk-pTA.SflXTU_sbIZL4E4IVyYIOKzwqNc", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "961827109012135966", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "eFddV6s28Kjvw2vWvmgCQc0DRTqfeJYm", //Discord Client Secret
+  ClientSecret: process.env.Discord_ClientSecret || "LlVseOq8KUNzlB3Vx5GYVzvB3ByEydNt", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   CallbackURL: "/api/callback", //Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
   "24/7": true, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "kanha", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
